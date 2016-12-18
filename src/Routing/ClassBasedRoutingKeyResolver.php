a55bd4807fbeaf8a4136f3dbd6dffece1490bd01
Menno Holtkamp <menno.holtkamp@gmail.com>
2016-12-18 19:08:20 +0100

    Update ClassBasedRoutingKeyResolver.php
    Allow other separator characters. Amazon SQS does not allow dots in queue names...

--- src/Routing/ClassBasedRoutingKeyResolver.php
+++ src/Routing/ClassBasedRoutingKeyResolver.php
@@ -4,11 +4,11 @@ namespace SimpleBus\Asynchronous\Routing;
 
 class ClassBasedRoutingKeyResolver implements RoutingKeyResolver
 {
-    public function resolveRoutingKeyFor($message)
+    public function resolveRoutingKeyFor($message, $separator = '.')
     {
         return str_replace(
             '\\',
-            '.',
+            $separator,
             get_class($message)
         );
     }
