b47ecc81a81c9e831993d209ea04a71ab7d7764a
Menno Holtkamp <menno.holtkamp@shopforce.nl>
2017-01-03 18:06:37 +0100

    Improved support for separator, prefix and suffix

--- src/Routing/ClassBasedRoutingKeyResolver.php
+++ src/Routing/ClassBasedRoutingKeyResolver.php
@@ -4,12 +4,48 @@ namespace SimpleBus\Asynchronous\Routing;
 
 class ClassBasedRoutingKeyResolver implements RoutingKeyResolver
 {
-    public function resolveRoutingKeyFor($message, $separator = '.')
+    /**
+     * @var string
+     */
+    private $prefix;
+
+    /**
+     * @var string
+     */
+    private $separator;
+
+    /**
+     * @var string
+     */
+    private $suffix;
+
+    /**
+     * @param string $separator
+     * @param string $prefix
+     * @param string $suffix
+     */
+    public function __construct($separator = '.', $prefix = null, $suffix = null)
     {
-        return str_replace(
-            '\\',
-            $separator,
-            get_class($message)
-        );
+        $this->separator = $separator;
+        if ($prefix !== null) {
+            $this->prefix = $prefix;
+        }
+        if ($suffix !== null) {
+            $this->suffix = $suffix;
+        }
+    }
+
+    public function resolveRoutingKeyFor($message)
+    {
+        $components = explode('\\', is_object($message) ? get_class($message) : $message);
+
+        if(isset($this->prefix)){
+            array_unshift($components, $this->prefix);
+        }
+        if(isset($this->suffix)){
+            $components[] = $this->suffix;
+        }
+
+        return implode($this->separator, $components);
     }
 }
